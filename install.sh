fe710c3df166c3e0eeb1d742a599d35f5f5b27ed
Revenberg <sander@revenberg.info>
2017-03-25 14:05:35 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,6 +12,12 @@ sudo apt-get install git -y
 sudo apt-get install python-pip git python-dev sshpass -y
 sudo pip install ansible
 
+sudo rm -rf ansible*
+git clone https://github.com/Revenberg/ansible.git
+git clone https://github.com/Revenberg/ansible-install.git
+git clone https://github.com/Revenberg/ansible-media.git
+git clone https://github.com/Revenberg/ansible-wifi.git
+
 # Configure IP address in "hosts" file. If you have more than one
 # Raspberry Pi, add more lines and enter details
 i=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
@@ -22,10 +28,6 @@ echo "$h ansible_host=$i" > hosts
 echo "[rpi]" > ~/ansible/hosts
 echo "$i  ansible_connection=ssh ansible_ssh_user=pi ansible_ssh_pass="$0 >> ~/ansible/hosts
 
-git clone https://github.com/Revenberg/ansible.git
-git clone https://github.com/Revenberg/ansible-install.git
-git clone https://github.com/Revenberg/ansible-media.git
-git clone https://github.com/Revenberg/ansible-wifi.git
 
 cd ~/ansible
 ansible-playbook setup.yml
