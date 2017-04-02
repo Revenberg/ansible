250a0b5cb0598938d3440252f1c192f3b3f3b459
Revenberg <sander@revenberg.info>
2017-04-02 19:56:15 +0200

    Change log file

--- install.sh
+++ install.sh
@@ -27,30 +27,30 @@ git clone https://github.com/Revenberg/ansible-bluetooth.git
 i=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
 h=$(hostname)
 
-echo "$h ansible_host=$i" > /home/pi/install.log
+echo "$h ansible_host=$i" > /home/pi/ansible.log
 
 echo "[rpi]" > ~/ansible/hosts
 echo "$i  ansible_connection=ssh ansible_ssh_user=pi ansible_ssh_pass="$1 >> ~/ansible/hosts
 
 cd ~/ansible
-ansible-playbook setup.yml >> /home/pi/install.log
+ansible-playbook setup.yml >> /home/pi/ansible.log
 
 cd ~/ansible-install
-ansible-playbook setup.yml >> /home/pi/install.log
+ansible-playbook setup.yml >> /home/pi/ansible.log
 
 cd ~/ansible-screen
-ansible-playbook setup.yml >> /home/pi/install.log
+ansible-playbook setup.yml >> /home/pi/ansible.log
 
 cd ~/ansible-wifi
-ansible-playbook setup.yml >> /home/pi/install.log
+ansible-playbook setup.yml >> /home/pi/ansible.log
 
 cd ~/ansible-media
-ansible-playbook setup.yml >> /home/pi/install.log
+ansible-playbook setup.yml >> /home/pi/ansible.log
 
 cd ~/ansible-kiosk
-ansible-playbook setup.yml >> /home/pi/install.log
+ansible-playbook setup.yml >> /home/pi/ansible.log
 
 cd ~/ansible-bluetooth
-ansible-playbook setup.yml >> /home/pi/install.log
+ansible-playbook setup.yml >> /home/pi/ansible.log
 
 sudo reboot
