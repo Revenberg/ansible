4aa1faa306f36ad1af81b0b3eba35d2954f9e63b
Revenberg <sander@revenberg.info>
2017-04-29 10:58:55 +0200

    Update install_volumio.sh

--- install_volumio.sh
+++ install_volumio.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-    echo $0: usage: ./install.sh  password
-    return 0
-fi
-
 sudo rm -rf /home/volumio/ansible*
 date >> /home/volumio/ansible.log
 
@@ -16,9 +11,18 @@ if (( "$i" == "0" )); then
         sudo sh -c "echo 'pi ALL=NOPASSWD: ALL' >> /etc/sudoers"
 fi
 
+i=$(sudo ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'| head -n1)
+h=$(hostname)
+
 sudo /bin/rm -v /etc/ssh/ssh_host_* -f
 sudo dpkg-reconfigure openssh-server
 sudo /etc/init.d/ssh restart
+
+ssh-keygen -t rsa -b 4096 -C "pi" -P "" -f "/home/volumio/.ssh/id_rsa" -q
+sudo cp /home/volumio/.ssh/id_rsa /home/pi/.ssh/id_rsa
+sudo chown pi:pi /home/volumio/.ssh/id_rsa
+
+ssh-keygen -f "/home/volumio/.ssh/known_hosts" -R $i
 
 sudo apt-get update
 sudo apt-get autoremove
@@ -34,10 +38,10 @@ git clone https://github.com/Revenberg/ansible.git
 git clone https://github.com/Revenberg/ansible-install.git
 git clone https://github.com/Revenberg/ansible-volumio-media.git
 
-echo "127.0.0.1 ansible_host=volumio" >> /home/volumio/ansible.log
+echo "$h ansible_host=$i" >> /home/volumio/ansible.log
 
 echo "[rpi]" > /home/volumio/ansible/hosts
-echo "volumio ansible_connection=ssh ansible_ssh_user=pi ansible_ssh_pass="$1 >> /home/volumio/ansible/hosts
+echo "$i ansible_connection=ssh ansible_ssh_user=pi ansible_ssh_pass="$1 >> /home/volumio/ansible/hosts
 
 cd /home/volumio/ansible-install
 ansible-playbook setup.yml  >> /home/volumio/ansible.log
