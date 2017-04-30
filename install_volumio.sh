2a1f31d857b4a79a96bf949b3b9539f480191b2c
Revenberg <sander@revenberg.info>
2017-04-30 11:13:48 +0200

    Update install_volumio.sh

--- install_volumio.sh
+++ install_volumio.sh
@@ -38,27 +38,3 @@ sudo /etc/init.d/ssh restart
 #sudo chown pi:pi /home/pi/.ssh/id_rsa
 
 #ssh-copy-id $i
-
-sudo apt-get update
-sudo apt-get autoremove
-
-sudo apt-get install git -y
-
-# Install Ansible and Git on the machine.
-sudo apt-get install python-pip git python-dev sshpass -y
-sudo pip install ansible
-sudo pip install markupsafe
-
-git clone https://github.com/Revenberg/ansible.git
-git clone https://github.com/Revenberg/ansible-install.git
-git clone https://github.com/Revenberg/ansible-volumio-media.git
-echo "$h ansible_host=$i" >> /home/volumio/ansible.log
-
-echo "[rpi]" > /home/volumio/ansible/hosts
-echo "$i ansible_connection=ssh ansible_ssh_user=pi ansible_ssh_pass=raspberry" >> /home/volumio/ansible/hosts
-
-cd /home/volumio/ansible-install
-ansible-playbook setup.yml  >> /home/volumio/ansible.log
-
-cd /home/volumio/ansible-volumio-media
-ansible-playbook setup.yml >> /home/volumio/ansible.log
