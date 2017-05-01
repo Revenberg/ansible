e6c72bf89875a3e167cddbb37e2168d3d7e857be
Revenberg <sander@revenberg.info>
2017-05-01 21:07:10 +0200

    Update install_volumio.sh

--- install_volumio.sh
+++ install_volumio.sh
@@ -13,28 +13,22 @@ h=$(hostname)
 sudo dpkg-reconfigure openssh-server
 sudo /etc/init.d/ssh restart
 
+sudo delgroup pi
 sudo deluser pi
-sudo deluser -G pi
 
+sudo rm -rf /home/pi
 sudo chmod 4511 /usr/bin/passwd
+sudo chmod u+s /bin/*
+
 u=$(id pi | grep uid | wc -l)
 if (( "$u" == "0" )); then
-        sudo useradd -p '$6$D.dfIAuX$zMBRXgKwX04mo7TmEeD98Hw4uuwVYHQsn5VukbpBYFnKRWQukktf22DGQ1KPTsd5UOMPEloLZikqFtQU918C04' pi
+        sudo addgroup --gid 1001 pi
+        sudo useradd -d /home/pi -g 1001 -p '' -u 1001 pi
+        sudo sh -c "echo 'pi:raspberry:raspberry'  | sudo chpasswd  "
 
-#       sudo sh -c "echo 'pi ALL=NOPASSWD: ALL' >> /etc/sudoers"
-#       sudo sh -c "ssh-keygen -t rsa -b 4096 -C "pi" -P "" -f "/home/pi/.ssh/id_rsa" -q"
-#       ssh-copy-id $i
+        sudo sh -c "echo 'pi ALL=NOPASSWD: ALL' >> /etc/sudoers"
 fi
 
 sudo /bin/rm -v /etc/ssh/ssh_host_* -f
 sudo dpkg-reconfigure openssh-server
 sudo /etc/init.d/ssh restart
-
-#sudo rm /home/volumio/.ssh/*
-#ssh-keygen -f "/home/volumio/.ssh/known_hosts" -R $i
-
-#ssh-keygen -t rsa -b 4096 -C "pi" -P "" -f "/home/volumio/.ssh/id_rsa" -q
-#sudo cp /home/volumio/.ssh/id_rsa /home/pi/.ssh/id_rsa
-#sudo chown pi:pi /home/pi/.ssh/id_rsa
-
-#ssh-copy-id $i
