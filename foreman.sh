4264b42608ec102891cc2f72a4b91a55708f0437
Revenberg <sander@revenberg.info>
2017-05-06 12:15:32 +0200

    Update foreman.sh

--- foreman.sh
+++ foreman.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo rm -rf ~/ansible* 2>/dev/null
-sudo rm -rf ~/foreman* 2>/dev/null
+sudo rm -rf ~/foreman-ansible* 2>/dev/null
 
 date >> ~/ansible.log
 sudo apt-get update 
@@ -19,7 +19,7 @@ git clone https://github.com/adfinis-sygroup/foreman-ansible.git
 i=$(sudo ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'| head -n1)
 h=$(hostname)
 
-echo "$i ansible_connection=ssh ansible_ssh_user=root ansible_ssh_pass=1234" > ~/foreman-ansible/ansible
+echo "$i ansible_connection=ssh ansible_ssh_user=root ansible_ssh_pass=1234" > /tmp/inventory
 cd ~/foreman-ansible/ansible
 
 ansible-playbook foreman.yml -i /tmp/inventory -u root
